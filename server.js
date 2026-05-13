e055f45144ca3073c6e7400d37f78f504d4c4a6e
kaleem ullah <kaleemullah399ktk@gmail.com>
2026-05-13 10:52:00 +0500

    fixed server for vercel

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
-import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// import dns from 'dns';
+// dns.setDefaultResultOrder('ipv4first');
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
+// import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import app from "./app.js";
+
+// dotenv.config();
+
+// // MongoDB Connection
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error("MongoDB Connection Error:", error);
+//     process.exit(1);
+//   }
+// };
+
+// connectDB();
+
+// // Start server
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+
+
+
+
 import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 import app from "./app.js";
 
-dotenv.config();
-
-// MongoDB Connection
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
-    process.exit(1);
   }
 };
 
 connectDB();
 
-// Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
-
-
-
+export default app;
